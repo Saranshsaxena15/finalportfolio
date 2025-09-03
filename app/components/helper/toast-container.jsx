cc9d09d998f32004ab3018225d25f6c08a1385ee
Saransh <saranshsaxena680@gmail.com>
2025-09-04 00:46:30 +0530

    Fix document is not defined error by creating client-side ToastContainer wrapper

---
+++ app/components/helper/toast-container.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ToastContainerWrapper = () => {
+  return <ToastContainer />;
+};
+
+export default ToastContainerWrapper;
